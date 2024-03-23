b20ff79320bf5632ba74cf838b69f41676e906f7
Profiremu23 <profirdani23@gmail.com>
2024-03-23 20:29:21 +0100

    Finished with Iteration 2 for the project (Cleaning up the zip codes)

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,9 +1,23 @@
+# frozen_string_literal: true
+
+require 'csv'
+
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5, '0')[0..4]
+end
+
 puts 'Event Manager Initialized!'
 
-lines = File.readlines('event_attendees.csv')
-lines.each_with_index do |line, index|
-  next if index == 0
-  columns = line.split(",")
-  name = columns[2]
-  p name
+contents = CSV.open(
+  'event_attendees.csv',
+  headers: true,
+  header_converters: :symbol
+)
+
+contents.each do |row|
+  name = row[:first_name]
+
+  zipcode = clean_zipcode(row[:zipcode])
+
+  puts "#{name} #{zipcode}"
 end
